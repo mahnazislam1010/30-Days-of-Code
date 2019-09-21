e489959feada8f72f59c9cecf1e605f59d3e6d2e
Mahnaz Rafia Islam <mahnazislam1@gmail.com>
2019-09-21 12:57:21 +0600

    solve using Python

---
+++ Day 2 - Operators.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(meal_cost, tip_percent, tax_percent):
+    tip = meal_cost * float(tip_percent/100)
+    tax = meal_cost * float(tax_percent/100)
+    result = round(meal_cost + tip + tax)
+    print(result)
+
+if __name__ == '__main__':
+    meal_cost = float(input())
+
+    tip_percent = int(input())
+
+    tax_percent = int(input())
+
+    solve(meal_cost, tip_percent, tax_percent)
+
+
+# In[ ]:
+
+
+
+
