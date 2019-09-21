c77c11649369de363054ca34ae6b846563f77634
Mahnaz Rafia Islam <mahnazislam1@gmail.com>
2019-09-21 12:29:07 +0600

    solve using Python

---
+++ Day 1 - Data Types.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+r = int(input())
+a = float(input())
+f = str(input())
+
+print(i+r)
+print(round(d+a,1))
+print(s + f)
+
